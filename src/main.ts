7924afecbc61820529d5e0bdfcf199c600ad459b
DylanA0110 <dylannjo0606@gmail.com>
2025-10-07 14:25:06 -0600

    enable CORS

--- src/main.ts
+++ src/main.ts
@@ -7,13 +7,19 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const logger = new Logger('bootstrap');
+  // Configurar CORS para permitir solicitudes desde el frontend Vite
+  app.enableCors({
+    origin: ['http://localhost:5173'], // URL de tu frontend Vite
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  });
 
-  app.setGlobalPrefix('api')
+  app.setGlobalPrefix('api');
   app.useGlobalPipes(
-  new ValidationPipe({
-    whitelist: true,
-    forbidNonWhitelisted: true,
-    })
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
   );
 
   const config = new DocumentBuilder()
@@ -21,15 +27,10 @@ async function bootstrap() {
     .setDescription('Taller endpoints')
     .setVersion('1.0')
     .build();
-    
+
   const documentFactory = () => SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, documentFactory);
 
-
-
-
   await app.listen(process.env.PORT ?? 3000);
-
-
 }
 bootstrap();
